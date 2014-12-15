402de727ccaa352a268af9d4f153ffa2d8c3f7c5
smakinson <smakinson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-15 16:51:01 +0000

    Forgot to add new view
    git-svn-id: https://plugins.svn.wordpress.org/goabroad-hq/trunk@1045114 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/activation_successful.php
@@ -0,0 +1,29 @@
+<div class="updated" style="padding: 0; margin: 0; border: none; background: none;">
+  <style type="text/css">
+.goabroadhq_activate {
+    min-width: 825px;
+    border: 1px solid #4F800D;
+    padding: 5px;
+    margin: 15px 0;
+    background: #83AF24;
+    background-image: -webkit-gradient(linear, 0% 0, 80% 100%, from(#83AF24), to(#4F800D));
+    background-image: -moz-linear-gradient(80% 100% 120deg, #4F800D, #83AF24);
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+    -webkit-border-radius: 3px;
+    position: relative;
+    overflow: hidden
+}
+.goabroadhq_activate .aa_description {
+    top: 22px;
+    left: 285px;
+    margin-left: 25px;
+    color: #E5F2B1;
+    font-size: 15px;
+    z-index: 1000
+}
+  </style>
+  <div class="goabroadhq_activate">
+    <div class="aa_description"><?php _e('Activation Successful', 'goabroadhq');?></div>
+  </div>
+</div>
