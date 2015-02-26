f7ff1f4586adcad239124c554fe8693846e3c1a1
smakinson <smakinson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-26 15:26:02 +0000

    Added the ability to set a completion redirect url
    git-svn-id: https://plugins.svn.wordpress.org/goabroad-hq/tags/0.4.1@1100430 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: smakinson
 Tags: goabroadhq
 Requires at least: 3.1
 Tested up to: 4.0.1
-Stable tag: 0.2.0
+Stable tag: 0.4.1
 License: GPLv2 or later
 
 A wordpress widget to add GoAbroadHQ fields to your website or blog. This plugin only works if you are an active client of GoAbroadHQ
@@ -20,6 +20,16 @@ Upload the GoAbroadHQ plugin to your blog, Activate it, then enter your GoAbroad
 
 == Changelog ==
 
+= 0.4.1 =
+* Added the ability to set a url to redirect to on completion.
+= 0.4.0 =
+* Added the ability to set the label for each row.
+= 0.3.2 =
+* Fixed a problem where the inital config page was limited to the number of characters entered.
+= 0.3.1 =
+* It now removes configuration from the database when the plugin is deactivated.
+= 0.3.0 =
+* Added reCaptcha support for the lead capture widget.
 = 0.2.0 =
 * Added the ability to set if a field is required as well as made it so you can delete a field from the widget. Also, all available HQ fields are now present in the lead capture.
 = 0.1.0 =
--- views/widget_admin.php
+++ views/widget_admin.php
@@ -3,11 +3,15 @@ if ( $instance ) {
   $title = $instance['title'];
   $rows = $instance['rows'];
   $required = $instance['required'];
+  $labels = $instance['labels'];
+  $redirect_url = $instance['redirect_url'];
 }
 else {
   $title = __( 'Lead Capture' , 'goabroadhq');
-  $rows = __( array('FirstName') , 'goabroadhq');
-  $required = __( array('FirstName'=>false) , 'goabroadhq');
+  $rows = __( array('Email') , 'goabroadhq');
+  $required = __( array('Email'=>true) , 'goabroadhq');
+  $labels = __( array('Email'=>'Email') , 'goabroadhq');
+  $redirect_url = __( '/' , 'goabroadhq');
 }
   $defaults = include(__DIR__.'/../sdk/defaults.php');
 ?>
@@ -52,18 +56,44 @@ else {
       .goabroadhq_options {
           float: left;
           padding: 6px;
+          display: block;
+      }
+      .goabroadhq_options_label {
+          display: block;
+          float: left;
+          width: 100%;
+      }
+
+      .goabroadhq_options_label > label {
+          width: 66px;
+          display: block;
+          float: left;
+          padding: 6px 0;
+      }
+
+      .goabroadhq_options_label > input {
+          border-radius: 5px;
+          display: block;
+          float: left;
+          width: 243px;
       }
     </style>
 <p>
 <label for="<?php echo $widget->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:' , 'goabroadhq'); ?></label>
 <input class="widefat" id="<?php echo $widget->get_field_id( 'title' ); ?>" name="<?php echo $widget->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+<label for="<?php echo $widget->get_field_id( 'redirect_url' ); ?>"><?php esc_html_e( 'Redirect Url:' , 'goabroadhq'); ?></label>
+<input class="widefat" id="<?php echo $widget->get_field_id( 'redirect_url' ); ?>" name="<?php echo $widget->get_field_name( 'redirect_url' ); ?>" type="text" value="<?php echo esc_attr( $redirect_url ); ?>" />
 <div id="lead_capture_rows">
   <label>Fields</label>
   <?php if(is_array($rows) && count($rows) > 0): ?>
     <?php foreach($rows as $row): ?>
       <div class="lead_capture_row">
+      <div class="goabroadhq_options_label">
+        <label>Label</label>
+        <input type="text" name="<?php echo $widget->get_field_name('labels'); ?>[<?=$row?>]" placeholder="Label" value="<?= $labels[$row] ?>" />
+      </div>
       <button class="delete_row">Delete</button>
-      <select class="goabroadhq_select" data-required="<?php echo $widget->get_field_name('required'); ?>" name="<?php echo $widget->get_field_name('rows'); ?>[]">
+      <select class="goabroadhq_select" data-required="<?php echo $widget->get_field_name('required'); ?>" data-label="<?php echo $widget->get_field_name('labels');?>" name="<?php echo $widget->get_field_name('rows'); ?>[]">
         <option value="">Select One</option>
         <?php foreach($HQ->getFieldTypes() as $key=>$val): ?>
           <option value="<?php echo $key ?>" <?php echo $key==$row ? 'selected' : null; ?>><?php echo $val?></option>
@@ -95,7 +125,8 @@ else {
       <?php endforeach; ?>
       var key = $(this).prev().children('select').attr('name');
       var required = $(this).prev().children('select').data('required');
-      $('<div class="lead_capture_row"><button class="delete_row">Delete</button><select name='+key+' data-required='+required+' class="goabroadhq_select">'+rows+'</select><div class="goabroadhq_options"></div></div>').insertBefore(this);
+      var label = $(this).prev().children('select').data('label');
+      $('<div class="lead_capture_row"><div class="goabroadhq_options_label"></div><button class="delete_row">Delete</button><select name='+key+' data-label="'+label+'" data-required='+required+' class="goabroadhq_select">'+rows+'</select><div class="goabroadhq_options"></div></div>').insertBefore(this);
     })
     $(document).on('click','.delete_row',function(){
       $(this).parent('.lead_capture_row').remove();
@@ -103,6 +134,8 @@ else {
     var defaults = JSON.parse('<?= json_encode($defaults) ?>');
     $('body').on('change','.goabroadhq_select',function(e){
       var required = $(this).data('required');
+      var label = $(this).data('label');
+      $(this).prev().prev('.goabroadhq_options_label').html('<label>Label</label><input type="text" name="'+label+'['+e.target.value+']" placeholder="Label" value="'+defaults['fields'][e.target.value].title+'" />');
       if(!defaults['fields'][e.target.value].required){
         $(this).next('.goabroadhq_options').html('<label><input name="'+required+'['+e.target.value+']" type="checkbox" /> required?</label>');
       } else {
